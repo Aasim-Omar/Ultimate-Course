3dfb4e84f5c5e7b50506bd11ccc6986d9c3af1f3
AasimOmar <aasim.omar7@gmail.com>
2021-02-16 13:52:58 +0200

    Lessons From 1 to 9

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require("gulp");
+var pug = require("gulp-pug");
+var sass = require("gulp-sass");
+var autoprefixer = require("gulp-autoprefixer");
+var concat = require("gulp-concat");
+var sourcemaps = require("gulp-sourcemaps");
+var minify = require("gulp-minify");
+var connect = require("gulp-connect");
+
+// HTML Task
+gulp.task("html", function () {
+  return gulp
+    .src("stage/html/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("./dist"))
+    .pipe(connect.reload())
+});
+
+// Css Task
+gulp.task("css", function () {
+  return gulp
+    .src("./stage/css/**/*.css")
+    .pipe(sourcemaps.init())
+    .pipe(concat("libs.css"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("./dist/css/"))
+});
+
+// Sass task
+gulp.task("sass", function() {
+  return gulp
+    .src("./stage/css/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(connect.reload());
+});
+
+// Js Task
+gulp.task("js", function () {
+  return gulp
+    .src("./stage/js/*.js")
+    .pipe(concat("main.js"))
+    .pipe(minify())
+    .pipe(gulp.dest("./dist/js/"))
+    .pipe(connect.reload());
+});
+
+// Warch Task
+gulp.task("watch", function() {
+  connect.server({root: "dist", livereload: true});
+  gulp.watch("./stage/html/**/*.pug", ["html"]);
+  gulp.watch("./stage/css/**/*.scss", ["sass"]);
+  gulp.watch("./stage/js/*.js", ["js"]);
+});
+
+// Default Task
+gulp.task("default", ["watch"]);
