10eab1c128ec1ad181eeeb91649357927f1e1635
AasimOmar <aasim.omar7@gmail.com>
2021-02-16 16:26:08 +0200

    Lesson 10

--- dist/js/main-min.js
+++ dist/js/main-min.js
@@ -1,1 +1,1 @@
-var x=20;
+document.querySelector(".toggle-sidebar").addEventListener("click",function(){document.querySelector(".sidebar").classList.toggle("no-sidebar"),document.querySelector(".content-area").classList.toggle("no-sidebar")});
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,5 @@
-var x = 20;
+// Toggled Sidebar
+document.querySelector(".toggle-sidebar").addEventListener("click", function() {
+  document.querySelector(".sidebar").classList.toggle("no-sidebar");
+  document.querySelector(".content-area").classList.toggle("no-sidebar");
+});
