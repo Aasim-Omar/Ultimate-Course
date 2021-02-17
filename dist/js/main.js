3914ed4ee680196e05a96c4bc2ba74fdf0800109
AasimOmar <aasim.omar7@gmail.com>
2021-02-17 21:14:05 +0200

    Lesson 14 Toggle Full Screen

--- dist/js/main.js
+++ dist/js/main.js
@@ -18,8 +18,6 @@ document.querySelector(".toggle-sidebar").addEventListener("click", function() {
 //   });
 // });
 
-
-
 document.querySelectorAll(".drop-down").forEach(function (drop) {
   drop.addEventListener("click", function () {
     if (this.children[1].clientHeight > 0) this.children[1].style.height = 0;
@@ -27,7 +25,6 @@ document.querySelectorAll(".drop-down").forEach(function (drop) {
     this.children[0].children[0].classList.toggle("rotate");
   });
 });
-
 
 // Notifaictions Toggle
 document.querySelector(".noti-toggle").addEventListener("click", function() {
@@ -37,6 +34,42 @@ document.querySelector(".noti-toggle").addEventListener("click", function() {
     this.nextElementSibling.style.height = `${this.nextElementSibling.scrollHeight}px`;
   }
 });
+
+// Toggle Full Screen
+document.querySelector(".toggle-full-screen").addEventListener("click", function() {
+  if(this.classList.contains("full-screen")) {
+    closeFullscreen();
+  } else {
+    openFullscreen();
+  }
+  this.classList.toggle("full-screen");
+});
+
+//Full Screen Functios
+var elem = document.documentElement;
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+// Close Full Screen
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    /* IE11 */
+    document.msExitFullscreen();
+  }
+}
 
 
 
